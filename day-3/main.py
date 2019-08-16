24b585678a7b97567bc999a60a49be7a712e3b99
Florent Decherf <f.decherf@tadaweb.com>
2019-08-16 15:29:38 +0200

    draft day 3

---
+++ day-3/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3.6
+
+
+def parse_input():
+    print("parse input")
+    with open('inputs/input_easy.txt') as input_file:
+        for line in input_file:
+            print(line.split())
+    _input = {}
+    return _input
+
+def init_map
+
+def main():
+    _input = parse_input()
+    print(_input)
+
+main()
