75c650676197614b742e876173fd59de58bf37bb
Florent Decherf <f.decherf@tadaweb.com>
2019-08-16 18:07:42 +0200

    draft day 3

--- day-3/main.py
+++ day-3/main.py
@@ -5,11 +5,28 @@ def parse_input():
     print("parse input")
     with open('inputs/input_easy.txt') as input_file:
         for line in input_file:
-            print(line.split())
-    _input = {}
+            _inputs = []
+            split = line.split()
+            print(split[2].split(',')[0])
+            print(split[2].split(',')[1].split(':')[0])
+            _x: int(split[2].split(',')[0])
+            _y: int(split[2].split(',')[1].split(':')[0])
+            _sizeX: int(split[3].split('x')[0])
+            _sizeY: int(split[3].split('x')[1])
+
+            _input = {
+                'x': _x
+                'y': _y
+                'sizeX': _sizeX
+                'sizeY': _sizeY
+            }
+#            _input.y = split[2].split(',')[1].split(':')[0]
+            print(_input)
+            
+            # print(split[3].split(','))
     return _input
 
-def init_map
+#def init_map
 
 def main():
     _input = parse_input()
